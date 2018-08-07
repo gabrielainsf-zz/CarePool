15f7b2b149ee0d2ed35b28ebe25154fc155d1545
vagrant <vagrant@vagrant.vm>
2018-08-07 00:11:30 +0000

    Created DB with fake seed data

---
+++ model.py
@@ -0,0 +1,106 @@
+"""Models and database functions for Rideshare project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# This is the connection to the PostgreSQL database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+##############################################################################
+# Model definitions
+
+class User(db.Model):
+    """User information."""
+
+    __tablename__ = "users"
+
+
+    # Log-in information
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String(64), nullable=True)
+    password = db.Column(db.String(64), nullable=True)
+
+
+    # Profile information
+    fname = db.Column(db.String(20), nullable=False)
+    lname = db.Column(db.String(20), nullable=False)
+    user_gender = db.Column(db.String(20), nullable=False)
+    user_bio = db.Column(db.String(160), nullable=False)
+    user_profile_img = db.Column(db.String(250), nullable=False)
+    user_social_media = db.Column(db.String(3000), nullable=False)
+
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return f"<User user_id={self.user_id} email={self.email}>"
+
+
+
+class Trip(db.Model):
+    """Trip information."""
+
+    __tablename__ = "trips"
+
+
+    trip_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    date_of_trip = db.Column(db.DateTime, nullable=False)
+    max_passengers = db.Column(db.Integer, nullable=False)
+    num_passengers = db.Column(db.Integer, nullable=False)
+    trip_cost = db.Column(db.Integer, nullable=False)
+
+    # User as Driver
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+
+    # Places
+    origin = db.Column(db.String(64), nullable=False)
+    destination = db.Column(db.String(64), nullable=False)
+
+    # Back reference to User
+    user = db.relationship("User", backref=db.backref("trips", order_by=trip_id))
+
+
+
+class UserTrip(db.Model):
+    """User (passenger) has joined a trip."""
+
+    __tablename__ = "user_trips"
+
+
+    user_trip_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+
+    # Back reference to Trip
+    trip_id = db.Column(db.Integer, db.ForeignKey('trips.trip_id'), nullable=False)
+    trip = db.relationship("Trip", backref=db.backref("user_trips", order_by=user_trip_id))
+
+    # User as Passenger
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+
+    # Back reference to User
+    user = db.relationship("User", backref=db.backref("user_trips", order_by=user_trip_id))
+
+
+
+##############################################################################
+# Helper functions
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///rideshares'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
+
---
+++ server.py
@@ -0,0 +1,87 @@
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, redirect, request, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import connect_to_db, db, User, Trip, UserTrip
+
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# Normally, if you use an undefined variable in Jinja2, it fails
+# silently. This is horrible. Fix this so that, instead, it raises an
+# error.
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Display Homepage."""
+
+    return render_template('homepage.html')
+
+@app.route('/login')
+def display_login_form():
+
+    return render_template('login_form.html')
+
+@app.route('/login', methods=["POST"])
+def login_process():
+
+    # Form data
+    email = request.form['email']
+    password = request.form['password']
+
+    # DB data
+    user_by_email = User.query.filter(User.email == email).first()
+    user_password = user_by_email.password
+    user_id = user_by_email.user_id
+
+    # Verify user is registered
+    if user_by_email is None:
+        flash('Oops! You need to register first.')
+    elif email == user_by_email.email and password == user_by_email.password:
+        session['user_id'] = user_id
+        flash('Yay! You are logged in.')
+
+    return redirect('/')
+
+@app.route('/rides')
+def display_trips():
+    """Display trips/rides available."""
+
+    return render_template('rides.html')
+
+@app.route('/add-ride')
+def add_trip():
+
+    return render_template('add_ride.html')
+
+@app.route('/add-ride', methods=["POST"])
+def add_trip_process():
+    """Adds a trip to the database."""
+
+    return redirect('/rides')
+
+
+
+
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+    # make sure templates, etc. are not cached in debug mode
+    app.jinja_env.auto_reload = app.debug
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
