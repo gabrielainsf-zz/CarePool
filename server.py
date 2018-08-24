8d089e957551bd17366a4f483c776c90bfad8f8a
vagrant <vagrant@vagrant.vm>
2018-08-24 20:35:42 +0000

    Created helpers directory and moved helper functions over

--- server.py
+++ server.py
@@ -5,13 +5,12 @@ More details here.
 """
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session
-from flask import json, jsonify
+from flask import jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from model import connect_to_db, db, User, Trip, UserTrip
-from googlemaps import convert
+from helpers import distance_matrix_filter
 from datetime import datetime, date
 from twilio.rest import Client
-import requests
 import os
 
 app = Flask(__name__)
@@ -191,53 +190,8 @@ def search_rides():
             return redirect('/search-rides')
 
         else:
-            trips_by_id = {trip.trip_id: trip for trip in trips}
-            possible_destinations = [trip.destination for trip in trips]
 
-            # Google Distance Matrix API set up
-            base_url = ('https://maps.googleapis.com/maps/api/distancematrix/'
-                        'json?')
-            payload = {
-                "origins": convert.location_list(destination),
-                "destinations": convert.location_list(possible_destinations)
-            }
-
-            r = requests.get(base_url, params=payload)
-
-            drop_off_distances = {}
-
-            if r.status_code != 200:
-                print('HTTP status code {} received, program terminated.'
-                      .format(r.status_code))
-            else:
-                response_dict = json.loads(r.text)
-                for offset, trip in enumerate(trips):
-                    cell = response_dict['rows'][0]['elements'][offset]
-                    if cell['status'] == 'OK':
-                        # Dictionary of drop off distances to key in by trip_id
-                        drop_off_distances[trip.trip_id] = cell['distance']['value']
-                        # print('{} to {}: {}.'
-                        #       .format(src, dst, cell['distance']['text']))
-                    else:
-                        print('{} to {}: status = {}'
-                              .format(origin, dst, cell['status']))
-
-            # print(drop_off_distances)
-            # print(possible_destinations)
-
-            drop_off_distances = {key:value for key, value in drop_off_distances
-                                  .items() if value <= 72420}
-
-            # print(drop_off_distances)
-
-            drop_offs_nearby = {}
-
-            for trip_idx in drop_off_distances:
-                trip = trips_by_id[trip_idx]
-                drop_offs_nearby[trip_idx] = trip
-
-            # print(drop_offs_nearby)
-
+            drop_offs_nearby = distance_matrix_filter(destination, trips)
             return render_template('nearby_search_results.html',
                                     origin=origin,
                                     destination=destination,
