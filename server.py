7285fd5318fef9af79c8d7958d2e5f87120d91de
vagrant <vagrant@vagrant.vm>
2018-09-07 23:43:00 +0000

    Added flask service file and turned off debug mode

--- server.py
+++ server.py
@@ -355,13 +355,11 @@ def logout():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = False
-    # make sure templates, etc. are not cached in debug mode
-    app.jinja_env.auto_reload = app.debug
+    # app.debug = False
+    # # make sure templates, etc. are not cached in debug mode
+    # app.jinja_env.auto_reload = app.debug
 
     connect_to_db(app)
 
-    # Use the DebugToolbar
-    DebugToolbarExtension(app)
 
     app.run(port=5000, host='0.0.0.0')
